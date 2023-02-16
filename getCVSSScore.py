e543520e4d8ae1c4c56e85129f0d30907aefc7d7
sammybolye <sammy.playstore@gmail.com>
2023-02-16 23:56:18 +0000

    Added Scanning files

---
+++ getCVSSScore.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3.7
+
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_cvss_score(cve_id, version):
+    # Define the URL for the NVD website
+    url = f"https://nvd.nist.gov/vuln/detail/{cve_id}"
+
+    if version == 'V2':
+        search_tag = "a#Cvss2CalculatorAnchor"
+    elif version == 'V3':
+        search_tag = "a#Cvss3NistCalculatorAnchor"    
+
+    # Make a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Find the CVSSv3 score and severity
+        cvss_element = soup.select_one(search_tag)
+        if cvss_element:
+            cvss_score = cvss_element.text.strip().split()[0]
+            cvss_severity = cvss_element.text.strip().split()[1]
+        else:
+            cvss_score = None
+            cvss_severity = None
+
+        # Return the results
+        return cvss_score, cvss_severity
+    else:
+        # Return None if the request was not successful
+        return None, None
+
+# Example usage
+#cve_id = "CVE-2021-23841"
+#cvssv2_score, cvssv2_severity = get_cvss_score(cve_id, 'V2')
+#cvssv3_score, cvssv3_severity = get_cvss_score(cve_id, 'V3')
+#print(f"CVSSv2 score for {cve_id}: {cvssv2_score} ({cvssv2_severity})")
+#print(f"CVSSv3 score for {cve_id}: {cvssv3_score} ({cvssv3_severity})")
---
+++ getcvebypackage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.7
+
+import requests
+from getCVSSScore import get_cvss_score
+
+
+# List of OpenSSL versions to retrieve CVEs for
+openssl_versions = ['1.0.2u', '1.1.0', '1.1.1k']
+#openssl_versions = ['1.0.2u']
+
+# Iterate over OpenSSL versions and retrieve related CVEs
+for version in openssl_versions:
+    # Construct API query URL
+    api_url = f'https://services.nvd.nist.gov/rest/json/cves/1.0?cpeMatchString=cpe:2.3:a:openssl:openssl:{version}:*'
+    
+    # Send API request and retrieve JSON response
+    response = requests.get(api_url)
+    response_json = response.json()
+    
+    #print(response_json)
+    # Extract relevant CVE information from response
+    cve_items = response_json['result']['CVE_Items']
+    cve_list = []
+    for cve_item in cve_items:
+        cve_id = cve_item['cve']['CVE_data_meta']['ID']
+        cve_description = cve_item['cve']['description']['description_data'][0]['value']
+        
+        #cve_impactV3 = cve_item['impact']['baseMetricV3']['cvssV3']['baseScore']
+        #cve_impactV2 = cve_item['impact']['baseMetricV2']['cvssV2']['baseScore']
+
+        cvssv2_score, cvssv2_severitiy  = get_cvss_score(cve_id, "V2")
+        cvssv3_score, cvssv3_severitiy  = get_cvss_score(cve_id, "V3")       
+       
+        cve_list.append((cve_id, cve_description, cvssv3_score, cvssv2_score, cvssv3_severitiy))
+        
+
+        
+        #print(f" The V2 score of {cve_id} is {cvssv2_score} {cvssv2_severitiy}")
+        #print(f" The V3 score of {cve_id} is {cvssv3_score} {cvssv3_severitiy}")
+    
+    #Print CVEs for the current OpenSSL version
+    print(f'\nCVEs for OpenSSL version {version}:\n--------------------------------')
+    for cve_id, cve_description, cvssv3_score, cvssv2_score, cvssv3_severitiy in cve_list:
+        print(f'{cve_id} | {cvssv2_score} | {cvssv3_score} | {cvssv3_severitiy}' )
+        #print(f'{cve_id} | {cvssv2_score} | {cvssv3_score} | {cvssv3_severitiy} | {cve_description}' )
